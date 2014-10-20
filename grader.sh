e5a2e29082cc07b684deb37972f9f26d003f3a10
Steve Cobb <scobb1@utexas.edu>
2014-10-20 01:23:05 -0500

    added mult cases

--- grader.sh
+++ grader.sh
@@ -1,10 +1,13 @@
 #Name:Steve Cobb
 #EID:scc2448
 
+# variables for use in grading
 filename='calculator.c'
 eid='EID:([a-zA-z][a-zA-Z]*[0-9][0-9]*)'
 addcases=('1' '1 1' '-1 -1' '123 456' '12 34 56 78' '1000000 1000000')
 addanswers=('1' '2' '-2' '579' '180' '2000000')
+multcases=('1' '10 10' '100 100')
+multanswers=('1' '100' '10000')
 
 for dir in $(ls); do
     if [[ -d "$dir" ]]; then
@@ -21,16 +24,26 @@ for dir in $(ls); do
                else
                    # run test cases
                    echo compile successful
+
+		   # usages cases
                    usage="$(./$dir/calculator.o)"
                    echo usage statement: $usage
-		   #echo adding 1
-                   #echo ${addcases[2]} | ./$dir/calculator.o add
-                   for i in "${!addcases[@]}"; do
+
+                   # add cases
+		   for i in "${!addcases[@]}"; do
                        answer=$(echo "${addcases[$i]}" | ./$dir/calculator.o add)
                        if [[ $answer = ${addanswers[$i]} ]]; then
                            echo got it
                        fi
                    done
+
+		   # mult cases
+		   for i in "${!multcases[@]}"; do
+                       answer=$(echo "${multcases[$i]}" | ./$dir/calculator.o multiply)
+                       if [[ $answer = ${multanswers[$i]} ]]; then
+		           echo got it
+	               fi
+	           done
                fi
 	    fi
         done
