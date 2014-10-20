e8ddf8af172ffca735a13103fa20efba8beaad0e
Steve Cobb <scobb1@utexas.edu>
2014-10-20 11:57:09 -0500

    Added functionality, started building result msg

--- grader.sh
+++ grader.sh
@@ -8,6 +8,11 @@ addcases=('1' '1 1' '-1 -1' '123 456' '12 34 56 78' '1000000 1000000')
 addanswers=('1' '2' '-2' '579' '180' '2000000')
 multcases=('1' '10 10' '100 100')
 multanswers=('1' '100' '10000')
+main='[[:space:]]*int[[:space:]][[:space:]]*main'
+commentregex='[[:space:]]*//.*'
+grader_comments=''
+prefix=''
+usage_word=('add', 'multiply', 'Usage', 'calculate')
 
 for dir in $(ls); do
     if [[ -d "$dir" ]]; then
@@ -18,33 +23,75 @@ for dir in $(ls); do
                    this_eid="${BASH_REMATCH[1]}"
 	       fi
                echo eid: $this_eid
+               if [[ -z $this_eid ]]; then
+                   resp=$dir
+	       else
+                   resp=$this_eid
+               fi	
+ 	       # comment check
+	       comments=0
+	       pre_main_flag=false
+	       while read p; do
+                   if [[ $p =~ $commentregex ]]; then
+                       let "comments = comments + 1"
+                   fi
+		   if [[ $p =~ $main ]]; then
+                       if [[ $prev =~ $commentregex ]]; then
+                           pre_main_flag=true
+                       fi
+                   fi
+                   prev=$p
+	       done < $dir/$file
+                   
+	       # compile
                gcc $dir/$filename -o$dir/calculator.o
                if [[ $? != 0 ]]; then
-                   echo compile failed
+		   # compile check
+                   grader_comments=$prefix$grader_commentscompile failed
+		   prefix=';'
+		   score=0
                else
+		   score=10
                    # run test cases
                    echo compile successful
 
 		   # usages cases
-                   usage="$(./$dir/calculator.o)"
-                   echo usage statement: $usage
+                   correct_usage=true
+                   test_usage="$(./$dir/calculator.o)"
+                   for word in $usage_words; do
+                       if [[ $test_usage != *$word* ]]; then
+			   echo $word not in $test_usage
+			   correct_usage=false
+   			   break;
+                       fi
+                   done
+
+		   echo correct_usage: $correct_usage
 
                    # add cases
 		   for i in "${!addcases[@]}"; do
                        answer=$(echo "${addcases[$i]}" | ./$dir/calculator.o add)
-                       if [[ $answer = ${addanswers[$i]} ]]; then
-                           echo got it
+                       if [[ $answer != ${addanswers[$i]} ]]; then
+                           let "score = score - 1"
                        fi
                    done
 
 		   # mult cases
 		   for i in "${!multcases[@]}"; do
                        answer=$(echo "${multcases[$i]}" | ./$dir/calculator.o multiply)
-                       if [[ $answer = ${multanswers[$i]} ]]; then
-		           echo got it
+                       if [[ $answer != ${multanswers[$i]} ]]; then
+		           let "score = score - 1"
 	               fi
 	           done
+                   echo score after add, mult: $score
                fi
+               resp=$resp,$score
+               if [[ $comments > 2 ]] && [[ $pre_main_flag = true ]]; then
+                   resp=$resp,ok
+               else
+                   resp=$resp,notok
+               fi
+               echo resp: $resp
 	    fi
         done
     fi
